e54da5b23fcee4a4b5361a1af318aedf538fc4df
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-10-21 17:54:26 -0300

    Requisito 1 concluido

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,51 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+export default class UserModel extends Model {
+  id!: number;
+  email!: string;
+  name!: string;
+  role!: string;
+  password!: string;
+}
+
+UserModel.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
