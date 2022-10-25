bd44e5ef05412df5a39a01d1ef7617efb3ad04cb
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-10-25 17:06:32 -0300

    Requisito 3 conluido mas não passa no teste

--- app/backend/src/middlewares/validadores.ts
+++ app/backend/src/middlewares/validadores.ts
@@ -2,7 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import * as Joi from 'joi';
 
 const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().email().required()
+    .messages({ 'any.required': 'All fields must be filled' }),
   password: Joi.string().min(6).required(),
 });
 
